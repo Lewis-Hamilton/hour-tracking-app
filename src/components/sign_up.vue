1ebb7f49711700c48acb267755b1577b30755e59
unknown <hamiltonjlewis@gmail.com>
2019-04-20 16:27:16 -0500

    added sign in button

--- src/components/sign_up.vue
+++ src/components/sign_up.vue
@@ -1,11 +1,16 @@
 <template>
     <div>
-        <b-container>
-          <b-img fluid src="https://firebasestorage.googleapis.com/v0/b/hour-tracking-app.appspot.com/o/foresight_logo.svg?alt=media&token=b490b5f7-c5b8-4a46-98c7-f417d28b7892"></b-img>
-          <b-form-input size="lg" v-model="email" type="email" placeholder="Email"></b-form-input>
-          <b-form-input size="lg" v-model="password" type="password" placeholder="Password"></b-form-input>
-          <b-button v-on:click="sign_up">Sign Up</b-button>
-        </b-container>
+      <b-container>
+        <b-img fluid src="https://firebasestorage.googleapis.com/v0/b/hour-tracking-app.appspot.com/o/foresight_logo.svg?alt=media&token=b490b5f7-c5b8-4a46-98c7-f417d28b7892"></b-img>
+        <b-form-input size="lg" v-model="email" type="email" placeholder="Email"></b-form-input>
+        <b-form-input size="lg" v-model="password" type="password" placeholder="Password"></b-form-input>
+        <b-button block variant="primary" v-on:click="sign_up">Sign Up</b-button>
+      </b-container>
+
+      <div id="signin">
+        <p class="float-left">Already have an account?</p>
+        <b-link class="float-right" to="sign_in">Sign In</b-link>
+      </div>
 </div>
 </template>
 
@@ -39,7 +44,7 @@ export default{
 
 <style scoped>
   .container{
-    margin: 175px auto;
+    margin-top: 175px;
   }
 
   .form-control{
@@ -49,6 +54,14 @@ export default{
   }
 
   .btn{
-    margin: 15px 150px;
+    margin: 15px 0px 15px 0px;
+  }
+
+  .float-left{
+    margin: 5px 5px 5px 30px;
+  }
+
+  .float-right{
+    margin: 5px 30px 5px 5px;
   }
 </style>
